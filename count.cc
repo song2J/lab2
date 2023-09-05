23232cdeb549c3a93a553ed9b995816e0017cb66
song2J <ejs1214@naver.com>
2023-09-05 19:53:49 +0900

    Modify count.cc

--- count.cc
+++ count.cc
@@ -1,6 +1,10 @@
 #include "count.h"
+#include <iostream>
+using namespace std;
 
 void count_number() {
     // lab2 //
     // do it yourself //
+    for(int i=1; i<=10; i++)cout<<i<<'\n';
+    cout<<"2021312868\n";
 }
